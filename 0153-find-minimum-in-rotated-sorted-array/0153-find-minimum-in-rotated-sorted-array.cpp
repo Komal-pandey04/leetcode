50fc20755424c48e579aed7be0b9dccf92a17c19
Komal Pandey <04komalpandey@gmail.com>
2026-09-09 22:40:43 +0530

    Time: 0 ms (100%), Space: 14.1 MB (49.65%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -8,11 +8,11 @@ public:
             int mid = left + (right - left) / 2;
 
             if (nums[mid] > nums[right]) {
-                // Minimum is in the right half
+                // Minimum is on the right side
                 left = mid + 1;
-            } 
+            }
             else {
-                // Minimum is at mid or in the left half
+                // Minimum is at mid or on the left side
                 right = mid;
             }
         }
